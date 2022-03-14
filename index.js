a782323783cf36c5437ca2ab7b6acf0e4c08e558
kysiam <yeasinsiam307@gmail.com>
2022-03-15 05:31:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+// var myName = "siam";
+// console.log(myName);
+// console.log(typeof(myName));
+// var myAge = "32";
+// var iAmSiam = false;
+// console.log(iAmSiam);
+// console.log(typeof(iAmSiam));
+
+// console.log(10+"20");
+
+// var myName = siam;
+// var myAge = "32";
+// console.log(myAge,);
+// console.log(typeof(iAmSiam));
+// console.log(NaN===NaN);
+// isNaN(NaN);
+// console.log(isNaN(Number.NaN));
+
+// console.log(5+20);
+// var a = 5;
+// var b = 20;
+// var c = a+b;
+// console.log(c);
+// console.log(typeof(c));
+
+
+// console.log(3+4);
+// console.log(2*4);
+// console.log(3-4);
+// console.log(3/4);
+// console.log("remainder operator" + 28%4);
+
+// console.log(3**3);
+// a= 5
+// b=10
+// output b=5 , a = 10
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// // console.log(c);
+// var a = c-a;
+// var b = c-a; 
+// console.log("the value of a is" + a);
+// console.log("the value of b is" + b);
+// // conwsole.log(a);
+// // console.log(b);
+// console.log(a);
+
+// debugger;
+// var year = 2022;
+// if( year % 4 === 0) {
+//     if(year % 100 === 0) {
+//         if(year % 400 === 0){
+//             console.log("this year " + year + "is a leap year");
+//         }else{
+//             console.log("this year " + year + "is not leap year"); 
+//         }
+//     }else{
+
+//         console.log("this year " + year + "is not leap year");   
+
+
+// }else{
+// console.log("this year " + year + "is not leap year");
+// }
+
+// var age = 20;
+// console.log(
+//     (age >= 20 ) ? " YOU CAN SEX " : " otherwise not "
+// );
+ 
+
+// var num = 0.12;
+// console.log(typeof(num));
+
+// var num1 = 50;
+// var num2 = 2;
+// if(num1 / num2 == 0){
+// console.log ("i am siam");
+// }else if(num1 % num2 ){
+// console.log("i am not siam");
+// }else  {
+// console.log("i am no more")
+// }
+// var myName = "siam"
+// var myName = 'sabbir vai';
+// var a=5, b=3 ;
+
+// switch(myName){
+//     case 'siam' :
+//         console.log("i am siam : " );
+//         break;
+//     case 'sabbir vai' :
+//         console.log( "i am not siam : " );
+//         break;
+//     default :
+//     console.log("i am no more :")
+// }
+
+// var num = 0 ;
+// while ( num >= 10){
+// console.log (num);
+// num++;
+// }
+// var num = 30 ;
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <= 50);
+ 
+// for(var num=30;num<=50;num++){
+//     console.log(num);
+// }
+// for(var num =1;num <= 10;num++ ){
+//     var tableof = 8;
+// console.log(tableof + "*" + num + "=" + tableof * num)
+// }
+
+// function sum(){
+//     var a = 2,b = 9;
+//     var total = a+b ;
+//     // console.log(total);
+//     return total;
+
+// }
+// // sum ();
+
+
+//   var x = 20;
+//   var y = sum();
+//   console.log(y + x);
+
+// function year(){
+//     var a = 2020;  b = 4;
+// if (a/b===0) {
+//     console.log("this is nlea pyear");
+// }else console.log("this is not");
+// }
+// year();
+// function year(someyear){
+ 
+// if (someyear%4===0 && someyear%100===0) {
+// return " a leap year"
+// }else {return " not leap year"}
+// }
+// var y=year(1000)
+// console.log(y);
+
+
+function sum(a,b){
+return total = a+b;
+}
+let v = sum("siam","shuvo");
+console.log(v);
